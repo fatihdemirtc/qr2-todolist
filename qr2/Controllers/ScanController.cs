12163b41c53f880db786df5f7c40ad0799432c66
Fatih Demir <fatihdemirtc@gmail.com>
2025-06-20 21:57:29 +0300

    ..

--- qr2/Controllers/ScanController.cs
+++ qr2/Controllers/ScanController.cs
@@ -32,15 +32,15 @@ namespace qr2.Controllers
 
             Console.WriteLine($"Scanned Product: {GetOS()}, Location: {GetBrowser()}, ip: { Request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP" }");
 
-
-
             Scan scan = new Scan
             {
                 ProductId = a.RecId,
-                Location = await GetCountry(),
+                Location = "unknown",
                 ScannedAt = DateTime.UtcNow,
-                ScannedDevice = 1,
-                Notes = ""
+                ScannedDevice = GetOS(),
+                Browser = GetBrowser(),
+                Notes = "",
+                IpAddress = Request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP"
             };
             _context.Scan.Add(scan);
             await _context.SaveChangesAsync();
@@ -50,17 +50,17 @@ namespace qr2.Controllers
             return View();
         }
 
-        public string GetOS()
+        public int GetOS()
         {
             string userAgent = Request.Headers["User-Agent"];
             var ua = userAgent.ToLower();
 
-            string os = "Unknown OS";
-            if (ua.Contains("windows")) os = "Windows";
-            else if (ua.Contains("mac")) os = "Mac";
-            else if (ua.Contains("linux")) os = "Linux";
-            else if (ua.Contains("android")) os = "Android";
-            else if (ua.Contains("iphone")) os = "iPhone";
+            int os = 0;
+            if (ua.Contains("windows")) os = 1;
+            else if (ua.Contains("mac")) os = 2;
+            else if (ua.Contains("linux")) os = 3;
+            else if (ua.Contains("android")) os = 4;
+            else if (ua.Contains("iphone")) os = 5;
             return os;
         }
 
@@ -74,34 +74,5 @@ namespace qr2.Controllers
             else if (ua.Contains("edge")) browser = "Edge";
             return browser;
         }
-
-        public async Task<string> GetCountry()
-        {
-            // Bu örnekte IP adresi üzerinden ülke bilgisi alınıyor.
-            // Gerçek uygulamada bir IP geolocation servisi kullanmanız gerekebilir.
-            string ip = Request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP";
-            
-            using var client = new HttpClient();
-            var response = await client.GetStringAsync($"https://ipapi.co/{ip}/json/");
-            dynamic locationInfo = JsonSerializer.Deserialize<dynamic>(response);
-
-            string city = locationInfo["city"];
-            string country = locationInfo["country_name"];
-
-            Console.WriteLine($"IP: {ip}, City: {city}, Country: {country}");
-
-            return country ?? "Unknown Country";
-        }
-    }
-    public class LoginLog
-    {
-        public int Id { get; set; }
-        public string UserId { get; set; }
-        public string IPAddress { get; set; }
-        public string OS { get; set; }
-        public string Browser { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
-        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
