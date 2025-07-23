27abbb2548de7baa71aa503a6bdebb2a6ebc15e0
Fatih Demir <fatihdemirtc@gmail.com>
2025-07-23 18:27:58 +0300

    ..

--- qr2/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ qr2/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -92,7 +92,7 @@ namespace qr2.Areas.Identity.Pages.Account
         public IActionResult OnPost(string provider, string returnUrl = null)
         {
             // Request a redirect to the external login provider.
-            var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
+            var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl }, protocol: "https");
             var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
             return new ChallengeResult(provider, properties);
         }
