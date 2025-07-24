72b7206704eca84a929e2ca8f02d0695c83eca19
Fatih Demir <fatihdemirtc@gmail.com>
2025-07-24 18:13:02 +0300

    ..

--- qr2/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ qr2/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -91,7 +91,7 @@ namespace qr2.Areas.Identity.Pages.Account
 
         public IActionResult OnPost(string provider, string returnUrl = null)
         {
-            var redirectUrl = "https://qrstyle.shop/signin-google";
+            var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
 
             _logger.LogInformation(redirectUrl + "");
 
